ccddf12ca15461c4f48b352058fdd3a279da018d
Prosefionol <ilya_kuper@mail.ru>
2025-02-09 22:23:52 +0300

    add stub to second fragment

--- app/src/main/java/com/example/catfightingclub/ui/FavoriteCatFragment.kt
+++ app/src/main/java/com/example/catfightingclub/ui/FavoriteCatFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.catfightingclub.databinding.FragmentFavoriteCatBinding
@@ -32,6 +33,14 @@ class FavoriteCatFragment : Fragment() {
 
         viewModel.favoriteCats.observe(viewLifecycleOwner) {
             adapter.submitList(it)
+            if (it.isEmpty()) {
+                binding.favoriteCatRv.isVisible = false
+                binding.stub.isVisible = true
+            }
+            else {
+                binding.favoriteCatRv.isVisible = true
+                binding.stub.isVisible = false
+            }
         }
 
         return binding.root
